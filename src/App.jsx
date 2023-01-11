e6e02e34bdff3d159e63d62332f8f3092960e9a9
midCenturyDude7 <mgriffes@gmail.com>
2023-01-10 21:59:31 -0800

    Added delete note functionality

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,11 @@ export default function App() {
     // }));
   }
   
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    setNotes(oldNotes => oldNotes.filter((note) => note.id !== noteId));
+  }
+
   function findCurrentNote() {
     return notes.find(note => {
       return note.id === currentNoteId;
@@ -80,6 +85,7 @@ export default function App() {
               currentNote={findCurrentNote()}
               setCurrentNoteId={setCurrentNoteId}
               newNote={createNewNote}
+              deleteNote={deleteNote}
             />
             {
               currentNoteId && 
